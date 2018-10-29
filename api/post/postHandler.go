a5ad88f6e861a6ae779f7238cef46a636a250be0
Unanoc <leedaniil98@gmail.com>
2018-10-29 23:35:52 +0300

    post update

--- api/post/postHandler.go
+++ api/post/postHandler.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"tpark_db/errors"
 	"tpark_db/helpers"
+	"tpark_db/models"
 
 	"github.com/valyala/fasthttp"
 )
@@ -36,5 +37,32 @@ func PostGetOneHandler(ctx *fasthttp.RequestCtx) {
 }
 
 func PostUpdateHandler(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("application/json")
 
+	postID := ctx.UserValue("id").(string)
+	postUpdate := models.PostUpdate{}
+	err := postUpdate.UnmarshalJSON(ctx.PostBody())
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusBadRequest) // 400 Bad Request
+		ctx.SetBodyString(err.Error())
+		return
+	}
+	result, err := helpers.PostUpdateHelper(&postUpdate, postID)
+
+	switch err {
+	case nil:
+		ctx.SetStatusCode(fasthttp.StatusOK) // 200
+		buf, _ := result.MarshalJSON()
+		ctx.SetBody(buf)
+	case errors.PostNotFound:
+		ctx.SetStatusCode(fasthttp.StatusNotFound) // 404
+		errorResp := errors.Error{
+			Message: fmt.Sprintf("Can't find post by id: %s", postID),
+		}
+		buf, _ := errorResp.MarshalJSON()
+		ctx.SetBody(buf)
+	default:
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError) // 500
+		ctx.SetBodyString(err.Error())
+	}
 }
--- helpers/postHelper.go
+++ helpers/postHelper.go
@@ -11,6 +11,7 @@ import (
 func PostFullHelper(id string, related []string) (*models.PostFull, error) {
 	postID, err := strconv.Atoi(id)
 	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 
@@ -18,8 +19,6 @@ func PostFullHelper(id string, related []string) (*models.PostFull, error) {
 
 	for _, typeObject := range related {
 		switch typeObject {
-		default:
-			postFull.Post, err = PostGetOneById(postID)
 		case "thread":
 			threadID := strconv.Itoa(postFull.Post.Thread)
 			postFull.Thread, err = GetThreadBySlugOrId(threadID)
@@ -29,10 +28,11 @@ func PostFullHelper(id string, related []string) (*models.PostFull, error) {
 		case "user":
 			userNickname := postFull.Post.Author
 			postFull.Author, err = UserGetOneHelper(userNickname)
+		default:
+			postFull.Post, err = PostGetOneById(postID)
 		}
 
 		if err != nil {
-			fmt.Println(err)
 			return nil, err
 		}
 	}
@@ -47,7 +47,7 @@ func PostGetOneById(id int) (*models.Post, error) {
 	post := models.Post{}
 
 	rows := tx.QueryRow(` 
-		SELECT id, author, message, forum, thread, created
+		SELECT id, author, message, forum, thread, created, isEdited
 		FROM posts
 		WHERE id = $1`,
 		id)
@@ -59,6 +59,7 @@ func PostGetOneById(id int) (*models.Post, error) {
 		&post.Forum,
 		&post.Thread,
 		&post.Created,
+		&post.IsEdited,
 	)
 
 	if err != nil {
@@ -67,4 +68,40 @@ func PostGetOneById(id int) (*models.Post, error) {
 
 	database.CommitTransaction(tx)
 	return &post, nil
+}
+
+func PostUpdateHelper(postUpdate *models.PostUpdate, postID string) (*models.Post, error) {
+	id, err := strconv.Atoi(postID)
+	if err != nil {
+		return nil, err
+	}
+
+	post, err := PostGetOneById(id)
+	if err != nil {
+		return nil, errors.PostNotFound
+	}
+
+	if len(postUpdate.Message) != 0 && (post.Message != postUpdate.Message) {
+		post.Message = postUpdate.Message
+		post.IsEdited = true
+	} else {
+		return post, nil
+	}
+
+	tx := database.StartTransaction()
+	defer tx.Rollback()
+
+	_, err = tx.Exec(`
+		UPDATE posts
+		SET message = coalesce(nullif($2, ''), message),
+			isEdited = TRUE
+		WHERE id = $1`,
+		postID, &postUpdate.Message)
+
+	if err != nil {
+		return nil, err
+	}
+
+	database.CommitTransaction(tx)
+	return post, nil
 }
