426f2024b25e0bf5903252f946fcce420934438a
Unanoc <leedaniil98@gmail.com>
2019-01-03 15:11:02 +0300

    rearranged URLs

--- router/router.go
+++ router/router.go
@@ -16,21 +16,21 @@ func NewRouter() *fasthttprouter.Router {
 	router.GET("/api/forum/:slug/threads", handlers.ForumGetThreadsHandler)
 	router.GET("/api/forum/:slug/users", handlers.ForumGetUsersHandler)
 
-	router.GET("/api/post/:id/details", handlers.PostGetOneHandler)
 	router.POST("/api/post/:id/details", handlers.PostUpdateHandler)
+	router.GET("/api/post/:id/details", handlers.PostGetOneHandler)
 
-	router.GET("/api/service/status", handlers.StatusHandler)
 	router.POST("/api/service/clear", handlers.ClearHandler)
+	router.GET("/api/service/status", handlers.StatusHandler)
 
-	router.GET("/api/thread/:slug_or_id/details", handlers.ThreadGetOneHandler)
-	router.GET("/api/thread/:slug_or_id/posts", handlers.ThreadGetPostsHandler)
 	router.POST("/api/thread/:slug_or_id/create", handlers.ThreadCreateHandler)
 	router.POST("/api/thread/:slug_or_id/details", handlers.ThreadUpdateHandler)
 	router.POST("/api/thread/:slug_or_id/vote", handlers.ThreadVoteHandler)
+	router.GET("/api/thread/:slug_or_id/details", handlers.ThreadGetOneHandler)
+	router.GET("/api/thread/:slug_or_id/posts", handlers.ThreadGetPostsHandler)
 
-	router.GET("/api/user/:nickname/profile", handlers.UserGetOneHandler)
 	router.POST("/api/user/:nickname/create", handlers.UserCreateHandler)
 	router.POST("/api/user/:nickname/profile", handlers.UserUpdateHandler)
+	router.GET("/api/user/:nickname/profile", handlers.UserGetOneHandler)
 
 	return router
 }
